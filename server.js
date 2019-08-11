ae4a9c0c31929a9c6cafd5dece2a04d1c13f551f
Alejandro González <alejandro@Alejandros-MacBook-Pro.local>
2019-08-11 15:37:38 +1200

    heroku setup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const path = require('path');
 
 //Load config file
 dotenv.config({ path: './config.env' });
@@ -13,6 +14,16 @@ if (process.env.NODE_ENV === 'development') {
 
 //Profile Routes
 app.use('/api/v1/profile', require('./routes/profile'));
+
+//Serve static assets
+
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 8000;
 
